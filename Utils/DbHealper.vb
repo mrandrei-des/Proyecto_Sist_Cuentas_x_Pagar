dbd5988dfb887f981bd96e8e8f6c709b874bdc96
Andrei Mesén Romero <mrandrei.dev@gmail.com>
2026-02-06 15:37:53 -0600

    Creación de clase conexión DB

---
+++ Utils/DbHealper.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Data.SqlClient
+
+Namespace Utils
+    Public Class DbHealper
+        Private connectionString As String = ConfigurationManager.ConnectionStrings("Sist_Cuentas_x_PagarConnectionString").ConnectionString
+
+        Public Function GetConnection() As SqlConnection
+            Return New SqlConnection(connectionString)
+        End Function
+    End Class
+End Namespace
