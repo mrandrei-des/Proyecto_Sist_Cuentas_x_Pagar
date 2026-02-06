f33d649c3b8a85f364a283072d3e8cd8e262143b
Andrei Mesén Romero <mrandrei.dev@gmail.com>
2026-02-06 15:26:04 -0600

    Creación de Clases

---
+++ Models/AplicarPago.vb
@@ -0,0 +1,120 @@
+﻿Namespace Models
+    Public Class AplicarPago
+        Private _numeroProveedor As Integer
+        Private _tipoFactura As String
+        Private _numeroFactura As String
+        Private _tipoDocumento As String
+        Private _numeroDocumento As String
+        Private _montoPagado As Double
+        Private _observacion As String
+        Private _fechaPago As Date
+        Private _moneda As String
+        Private _tipoCambio As Double
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(numeroProveedor As Integer, tipoFactura As String, numeroFactura As String, tipoDocumento As String, numeroDocumento As String, montoPagado As Double, observacion As String, fechaPago As Date, moneda As String, tipoCambio As Double)
+            Me.NumeroProveedor = numeroProveedor
+            Me.TipoFactura = tipoFactura
+            Me.NumeroFactura = numeroFactura
+            Me.TipoDocumento = tipoDocumento
+            Me.NumeroDocumento = numeroDocumento
+            Me.MontoPagado = montoPagado
+            Me.Observacion = observacion
+            Me.FechaPago = fechaPago
+            Me.Moneda = moneda
+            Me.TipoCambio = tipoCambio
+        End Sub
+
+        Public Property NumeroProveedor As Integer
+            Get
+                Return _numeroProveedor
+            End Get
+            Set(value As Integer)
+                _numeroProveedor = value
+            End Set
+        End Property
+
+        Public Property TipoFactura As String
+            Get
+                Return _tipoFactura
+            End Get
+            Set(value As String)
+                _tipoFactura = value
+            End Set
+        End Property
+
+        Public Property NumeroFactura As String
+            Get
+                Return _numeroFactura
+            End Get
+            Set(value As String)
+                _numeroFactura = value
+            End Set
+        End Property
+
+        Public Property TipoDocumento As String
+            Get
+                Return _tipoDocumento
+            End Get
+            Set(value As String)
+                _tipoDocumento = value
+            End Set
+        End Property
+
+        Public Property NumeroDocumento As String
+            Get
+                Return _numeroDocumento
+            End Get
+            Set(value As String)
+                _numeroDocumento = value
+            End Set
+        End Property
+
+        Public Property MontoPagado As Double
+            Get
+                Return _montoPagado
+            End Get
+            Set(value As Double)
+                _montoPagado = value
+            End Set
+        End Property
+
+        Public Property Observacion As String
+            Get
+                Return _observacion
+            End Get
+            Set(value As String)
+                _observacion = value
+            End Set
+        End Property
+
+        Public Property FechaPago As Date
+            Get
+                Return _fechaPago
+            End Get
+            Set(value As Date)
+                _fechaPago = value
+            End Set
+        End Property
+
+        Public Property Moneda As String
+            Get
+                Return _moneda
+            End Get
+            Set(value As String)
+                _moneda = value
+            End Set
+        End Property
+
+        Public Property TipoCambio As Double
+            Get
+                Return _tipoCambio
+            End Get
+            Set(value As Double)
+                _tipoCambio = value
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ Models/Usuario.vb
@@ -0,0 +1,87 @@
+﻿Namespace Models
+    Public Class Usuario
+        Private _nombreUsuario As String
+        Private _constrasenna As String
+        Private _nombre As String
+        Private _apellido1 As String
+        Private _apellido2 As String
+        Private _correo As String
+        Private _estado As Integer
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(nombreUsuario As String, constrasenna As String, nombre As String, apellido1 As String, apellido2 As String, correo As String, estado As Integer)
+            Me.NombreUsuario = nombreUsuario
+            Me.Constrasenna = constrasenna
+            Me.Nombre = nombre
+            Me.Apellido1 = apellido1
+            Me.Apellido2 = apellido2
+            Me.Correo = correo
+            Me.Estado = estado
+        End Sub
+
+        Public Property NombreUsuario As String
+            Get
+                Return _nombreUsuario
+            End Get
+            Set(value As String)
+                _nombreUsuario = value
+            End Set
+        End Property
+
+        Public Property Constrasenna As String
+            Get
+                Return _constrasenna
+            End Get
+            Set(value As String)
+                _constrasenna = value
+            End Set
+        End Property
+
+        Public Property Nombre As String
+            Get
+                Return _nombre
+            End Get
+            Set(value As String)
+                _nombre = value
+            End Set
+        End Property
+
+        Public Property Apellido1 As String
+            Get
+                Return _apellido1
+            End Get
+            Set(value As String)
+                _apellido1 = value
+            End Set
+        End Property
+
+        Public Property Apellido2 As String
+            Get
+                Return _apellido2
+            End Get
+            Set(value As String)
+                _apellido2 = value
+            End Set
+        End Property
+
+        Public Property Correo As String
+            Get
+                Return _correo
+            End Get
+            Set(value As String)
+                _correo = value
+            End Set
+        End Property
+
+        Public Property Estado As Integer
+            Get
+                Return _estado
+            End Get
+            Set(value As Integer)
+                _estado = value
+            End Set
+        End Property
+    End Class
+End Namespace
