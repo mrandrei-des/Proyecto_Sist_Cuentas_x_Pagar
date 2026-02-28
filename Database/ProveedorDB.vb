85366a9ac3bbeccd4657d716ad730d7fb9373978
Andrei Mesén Romero <mrandrei.dev@gmail.com>
2026-02-28 12:37:20 -0600

    Se hacen cambios en los nombres de los procedimientos almacenados y algunos parámetros.

--- Database/ProveedorDB.vb
+++ Database/ProveedorDB.vb
@@ -6,7 +6,7 @@ Public Class ProveedorDB
     'Función que tiene el query para agregar un proveedor nuevo
     Public Function CrearProveedor(ByVal objProveedor As Models.Proveedor, ByVal usuarioInserta As String, ByRef errorMessage As String) As Boolean
         Using db.GetConnection()
-            Dim query As String = "EXEC sp_Insertar_Proveedor_Nuevo @Nombre, @TipoIdentificacion, @Identificacion, @CorreoElectronico, @Estado, @UsuarioCreacion "
+            Dim query As String = "EXEC sp_Inserta_Proveedor_Nuevo @Nombre, @TipoIdentificacion, @Identificacion, @CorreoElectronico, @Estado, @UsuarioCreacion "
             Dim parameters As New Dictionary(Of String, Object) From {
                 {"@Nombre", objProveedor.Nombre},
                 {"@TipoIdentificacion", objProveedor.TipoIdentificacion},
@@ -22,9 +22,9 @@ Public Class ProveedorDB
 
     'Función que tiene el query para eliminar un usuario
     Public Function EliminarProveedor(idProveedorAfectado As Integer, usuarioElimino As String, ByRef errorMessage As String) As Boolean
-        Dim query As String = "EXEC sp_Eliminar_Proveedor @idProveedorAfectado, @UsuarioElimino "
+        Dim query As String = "EXEC sp_Eliminar_Proveedor @ID_Proveedor, @UsuarioElimino "
         Dim parameters As New Dictionary(Of String, Object) From {
-            {"@idProveedorAfectado", idProveedorAfectado},
+            {"@ID_Proveedor", idProveedorAfectado},
             {"@UsuarioElimino", usuarioElimino}
         }
         Return db.ExecuteNonQuery(query, parameters, errorMessage)
@@ -32,8 +32,9 @@ Public Class ProveedorDB
 
     Public Function ModificarProveedor(ByVal objProveedor As Models.Proveedor, ByVal usuarioModifico As String, ByRef errorMessage As String) As Boolean
         Using db.GetConnection()
-            Dim query As String = "EXEC sp_Modificar_Proveedor @Nombre, @TipoIdentificacion, @Identificacion, @CorreoElectronico, @Estado, @UsuarioModifico "
+            Dim query As String = "EXEC sp_Modificar_Proveedor @ID_ProveedorAfectado, @Nombre, @TipoIdentificacion, @Identificacion, @CorreoElectronico, @Estado, @UsuarioModifico "
             Dim parameters As New Dictionary(Of String, Object) From {
+                {"@ID_ProveedorAfectado", objProveedor.NumeroProveedor},
                 {"@Nombre", objProveedor.Nombre},
                 {"@TipoIdentificacion", objProveedor.TipoIdentificacion},
                 {"@Identificacion", objProveedor.NumeroIdentificacion},
